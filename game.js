b9ab10aa2c91f3e366be825cdcdc7a983f83b071
jonpfluger <jonpfluger718@gmail.com>
2023-06-12 21:33:00 -0500

    play again while loop

--- game.js
+++ game.js
@@ -2,39 +2,42 @@ var wins = 0
 var losses = 0
 var ties = 0
 
+var playAgain = true
+
 var choices = ["R", "P", "S"]
 
-// human choice
-var humanChoice = prompt("Choose R, P, or S")
+while (playAgain){
+    // human choice
+    var humanChoice = prompt("Choose R, P, or S")
 
-// random computer choice
-var randomNum = Math.floor(Math.random() * 3)
-var computerChoice= choices[randomNum]
+    // random computer choice
+    var randomNum = Math.floor(Math.random() * 3)
+    var computerChoice= choices[randomNum]
 
-console.log("Human: " + humanChoice)
-console.log("Computer: " + computerChoice)
+    alert("Human: " + humanChoice + "\nComputer: " + computerChoice)
 
-// compare choices
-if (
-    humanChoice === "S" && computerChoice === "P" ||
-    humanChoice === "R" && computerChoice === "S" ||
-    humanChoice === "P" && computerChoice === "R"
-) {
-    alert("You win!")
-    wins++
-} else if (humanChoice === computerChoice) {
-    alert("It's a tie!")
-    ties++
-} else {
-    alert("You lost!")
-    losses++
+    // compare choices
+    if (
+        humanChoice === "S" && computerChoice === "P" ||
+        humanChoice === "R" && computerChoice === "S" ||
+        humanChoice === "P" && computerChoice === "R"
+    ) {
+        alert("You win!")
+        wins++
+    } else if (humanChoice === computerChoice) {
+        alert("It's a tie!")
+        ties++
+    } else {
+        alert("You lost!")
+        losses++
+    }
+
+    // show score
+    alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
+
+    // play again?
+    playAgain = confirm("Play again?")
+
 }
 
-// show score
-alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
-
-// play again?
-    // if yes
-        // restart game
-    // if no
-        // exit game
+alert("Thanks for playing!")
