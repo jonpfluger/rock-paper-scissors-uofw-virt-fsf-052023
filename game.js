b771cbe642add5bffc34ecf0c9ccf1b0b2a2259e
jonpfluger <jonpfluger718@gmail.com>
2023-06-12 20:34:49 -0500

    pseudocode

--- game.js
+++ game.js
@@ -1,1 +1,28 @@
-console.log("Hello RPS")
+
+// variables
+    // num wins
+    // num losses
+    // num ties
+
+// players choose R, P, or S
+    // get human choice from input
+    // computer randomly chooses
+
+// compare choices
+    // if humanChoice is S and computerChoice is P OR
+    // if humanChoice is R and computerChoice is S OR
+    // if humanChoice is P and computerChoice is R 
+        // alert human wins
+        // increase wins
+    // if humanChoice === computerChoice
+        // alert tie
+        // increase ties
+    // else
+        // alert that lost
+        // increase losses
+
+// play again?
+    // if yes
+        // restart game
+    // if no
+        // exit game
